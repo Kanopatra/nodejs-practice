abf23342aa5742f00522a1286063664686ef31cd
macos <macos@macoss-MacBook-Pro.local>
2026-05-05 14:31:11 +0530

    create server using http

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 // const math=require('./math');
 // console.log( "Add value",math.add(3,5),math.subtract(10,4));
 // Second approch  =========
-const {add,subtract}=require('./math');
-console.log( "Add value",add(3,5),subtract(10,4));
+// const {add,subtract}=require('./math');
+// console.log( "Add value",add(3,5),subtract(10,4));
+
+import  http from 'http';
+import fs from 'fs';
+const server=http.createServer((req,res)=>{{
+   const logs= fs.appendFile('requests.log', `${req.method} ${req.url}\n`, (err) => {});
+console.log(`Received ${req.method} request for ${req.url}`);
+res.end("Welcome to Node.js server!")
+
+
+}})
+server.listen(3000);
