e5d3934d8871bf47524a0ebb9b24051bffb23a1d
macos <macos@macoss-MacBook-Pro.local>
2026-05-05 12:24:34 +0530

    day 1: node basics, modules and fs

---
+++ file.js
@@ -0,0 +1,22 @@
+const fs=require('fs');
+// fs.writeFileSync('text.txt','Hello Node.js with Sync');
+// fs.writeFile('text.txt','Hello Node.js with Async',(err)=>{});
+
+// const result = fs.readFileSync('text.txt', 'utf8');
+// console.log(result);
+
+// fs.readFile('text.txt', 'utf8', (err, data) => {  
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     else{
+//           console.log(data);
+//     }
+  
+//   });
+
+// fs.appendFileSync('text.txt', '\nAppended text with Async');
+// console.log(fs.statSync('text.txt'));  
+// fs.copyFileSync('text.txt','text_copy.txt');      
+// fs.unlinkSync('text_copy.txt');                                                                        
---
+++ index.js
@@ -0,0 +1,6 @@
+// =======first approch
+// const math=require('./math');
+// console.log( "Add value",math.add(3,5),math.subtract(10,4));
+// Second approch  =========
+const {add,subtract}=require('./math');
+console.log( "Add value",add(3,5),subtract(10,4));
