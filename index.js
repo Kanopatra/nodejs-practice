765117c83106d2b9e3f49b25c3412d223388af3a
macos <macos@macoss-MacBook-Pro.local>
2026-05-08 10:44:25 +0530

    feat: implemented CRUD APIs with middleware and mock data

--- index.js
+++ index.js
@@ -46,19 +46,107 @@
 
 
 
-import express from 'express'
+// import express from 'express'
 
 
 
-const App=express()
+// const App=express()
 
-App.get("/",(req, res)=>{
-    return res.send(`Hello welcome to home `)
+// App.get("/",(req, res)=>{
+//     return res.send(`Hello welcome to home `)
+// })
+//  App.get("/about",(req, res)=>{
+//     console.log("url====",req.query)
+//     return res.send(`Hello welcome to home ${req.query.name}`)
+// })
+
+
+// App.listen(3000,()=>console.log(" server started"))
+
+import express from 'express';
+import users from './MOCK_DATA.json' with { type: 'json' };
+import fs from 'fs'
+const app=express();
+
+const PORT =3000;
+//Middleware
+app.use(express.urlencoded({extended:false}))
+app.get('/api/users',(req,res)=>{
+  return  res.json(users)}
+)
+
+// app.get('/api/users/:id',(req,res)=>{
+//     const id=Number(req.params.id);
+//     console.log("id ",id)
+//     const user=users.find(users=>users.id===id)
+//     return res.json(user)
+// })
+
+
+// app.patch('/api/users/:id',(req,res)=>{
+
+
+//     return res.send("pending")
+// })
+
+
+
+// app.delete('/api/users/:id',(req,res)=>{
+
+
+//     return res.send("pending")
+// })
+
+app.post('/api/users',(req,res)=>{
+    const user=req.body;
+        console.log("Body",user)
+    users.push({...user,id:users.length+1})
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+       return res.json({status:"successfull",id:users.length })  
+    })
+
+   
 })
- App.get("/about",(req, res)=>{
-    console.log("url====",req.query)
-    return res.send(`Hello welcome to home ${req.query.name}`)
+
+
+//here '/api/users/:id' is common for all method so we need to merge it
+
+app.route('/api/users/:id')
+
+.get((req, res) => {
+    const id = Number(req.params.id);
+
+    const user = users.find(
+      user => user.id === id
+    );
+
+    return res.json(user);
+})
+
+.patch((req, res) => {
+    const id=Number(req.params.id);
+ const user = users.find(user => user.id === id);
+if(!user){
+       return res.json({ status: "user not found" });
+}
+ user.first_name=req.body.first_name|| user.first_name;
+ user.last_name = req.body.last_name || user.last_name;
+    user.email = req.body.email || user.email;
+
+    return res.json({ status: "Updated Successfully",id:user.id });
 })
 
 
-App.listen(3000,()=>console.log(" server started"))
+.delete((req, res) => {
+    const id= Number(req.params.id);
+    const index=users.findIndex(users=>users.id===id)
+      const deletedUser = users[index];
+users.splice(index,1)
+    return res.json({ status: "user Deleted successfully",id:deletedUser.id});
+});
+
+
+app.listen(PORT,()=>{
+    console.log("server started on PORT",PORT)
+})
+
---
+++ task.tsx
@@ -0,0 +1,18 @@
+REST API -RESPONSE
+
+
+GET /api/users -- list all the users json 
+GET /api/users/1 - get users with ID 1
+GET /api/users/2- get users with ID 2
+
+POST /api/users --   create users
+
+PATCH /api/users/1 --- edit users with Id 1
+
+
+
+Dynamic path parameters
+
+GET /api/users/:id
+
+:id -->variable/Dynamic
